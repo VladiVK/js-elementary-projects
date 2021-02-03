d06b7bf3d1ae0daf60ea1735debb69f3273b7df7
VladiVK <kozobon@gmail.com>
2021-02-03 10:39:54 +0200

    completed color-flipper

--- 1-color-flipper/app.js
+++ 1-color-flipper/app.js
@@ -1,1 +1,34 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgb(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomNumber = getRandomNumber();
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+// btn.addEventListener('click', () => {
+//   const newBgColor = getRandomColor();
+
+//   document.body.style.backgroundColor = newBgColor;
+//   color.textContent = newBgColor;
+// });
+
+// function getRandomColor() {
+//   let lastColor = document.body.style.backgroundColor;
+//   let newColor = lastColor;
+
+//   do {
+//     let randomNumber = Math.floor(Math.random() * colors.length);
+//     newColor = colors[randomNumber];
+//   } while (lastColor === newColor);
+
+//   return newColor;
+// }
