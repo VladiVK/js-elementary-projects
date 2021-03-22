7c61612d5a4e8aea9fc5c4283de69944f667521c
VladiVK <kozobon@gmail.com>
2021-03-22 21:53:43 +0200

    Cocktails: setDrink with localStorage

--- 20-cocktails/src/presentDrinks.js
+++ 20-cocktails/src/presentDrinks.js
@@ -1,11 +1,18 @@
 import fetchDrinks from './fetchDrinks.js';
 import displayDrinks from './displayDrinks.js';
+import setDrink from './setDrink.js';
 
 const presentDrinks = async (url) => {
-  // fetch drinks
+  // *** fetch drinks
   const data = await fetchDrinks(url);
-  // display drinks
+  // *** display drinks
+  // технически ждать необязательно,
+  // но в нашем случае - важно, так как будем потом динамически
+  //переходить на отдельные страницы
   const section = await displayDrinks(data);
+  if (section) {
+    setDrink(section);
+  }
 };
 
 export default presentDrinks;
