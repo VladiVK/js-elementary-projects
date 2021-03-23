8f572dc62841d1f11d725aee9128fbc2370eadfd
VladiVK <kozobon@gmail.com>
2021-03-23 16:44:23 +0200

    Cocktails is completed

--- 20-cocktails/singleDrink.js
+++ 20-cocktails/singleDrink.js
@@ -1,3 +1,14 @@
-const id = JSON.parse(localStorage.getItem('drink'));
-console.log(id);
-const urlID = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`;
+import fetchDrinks from './src/fetchDrinks.js';
+import displayDrink from './src/displaySingleDrink.js';
+
+const presentDrink = async () => {
+  const id = JSON.parse(localStorage.getItem('drink'));
+  if (!id) {
+    window.location.replace('index.html');
+  } else {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const drink = await fetchDrinks(url);
+    displayDrink(drink);
+  }
+};
+window.addEventListener('DOMContentLoaded', presentDrink);
--- 20-cocktails/src/displaySingleDrink.js
+++ 20-cocktails/src/displaySingleDrink.js
@@ -1,3 +1,38 @@
-const id = localStorage.getItem('drink');
-console.log(id);
-const urlID = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`;
+import { hideLoading } from './toggleLoading.js';
+import get from './getElement.js';
+
+const displayDrink = (data) => {
+  hideLoading();
+  const drink = data.drinks[0];
+  const {
+    idDrink: id,
+    strDrink: name,
+    strInstructions: desc,
+    strDrinkThumb: image,
+  } = drink;
+  const ingredientsList = [
+    drink.strIngredient1,
+    drink.strIngredient2,
+    drink.strIngredient3,
+    drink.strIngredient4,
+    drink.strIngredient5, //null
+    drink.strIngredient6, //null
+  ];
+
+  const img = get('.drink-img');
+  const drinkName = get('.drink-name');
+  const decription = get('.drink-desc');
+  const ingredients = get('.drink-ingredients');
+  img.src = image;
+  drinkName.textContent = name;
+  decription.textContent = desc;
+  ingredients.innerHTML = ingredientsList
+    .map((item) => {
+      if (!item) return;
+      return `<li><i class="far fa-check-square"></i>${item}</li>`;
+    })
+    .join('');
+  document.title = `Cocktail ${name}`;
+  console.log(drink, ingredientsList);
+};
+export default displayDrink;
